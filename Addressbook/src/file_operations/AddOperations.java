da7c966d693f22c674e9f7914d016ce9e5497d3b
Tharun B <67778546+sudo-tharun@users.noreply.github.com>
2022-01-11 22:25:36 +0530

    Add operations

--- Addressbook/src/file_operations/AddOperations.java
+++ Addressbook/src/file_operations/AddOperations.java
@@ -9,7 +9,7 @@ public class AddOperations{
 	
 	//function to display existing address books
 	public static String [] displayAddBooks(){
-		File display= new File("C:\\Users\\Kapture\\eclipse-workspace\\Addressbook\\address_books");
+		File display= new File("Path");
 		list_of_files=display.list();
 		System.out.println("\r\nAvailable addressbooks are:");
 		for(int i=0;i<list_of_files.length;i++)
@@ -21,7 +21,7 @@ public class AddOperations{
 	public static void addAddrBook() {
 		System.out.println("\rEnter the name of the addressbook: \n>");
 		String addr_book=sc.nextLine();
-		File addr=new File("C:/Users/Kapture/eclipse-workspace/Addressbook/address_books/"+addr_book+".txt");
+		File addr=new File("Path"+addr_book+".txt");
 		if(!addr.exists()) {
 			try {
 				addr.createNewFile();
@@ -46,7 +46,7 @@ public class AddOperations{
 		int ch_add=sc.nextInt();
 		
 		try {
-			FileWriter myWriter = new FileWriter("C:/Users/Kapture/eclipse-workspace/Addressbook/address_books/"+list_of_files[ch_add-1],true);
+			FileWriter myWriter = new FileWriter("Path"+list_of_files[ch_add-1],true);
 	    	myWriter.write(fname+","+lname+","+ph+","+email+","+addr+".\n");
 	    	myWriter.close();
 	    	System.out.print("\r\nDetails successfully added to "+list_of_files[ch_add-1]+" !");
